3bbf5a5df161c95d009d4bf17f32f5500ae73788
LukaRapaic <luka.rapaic.fsra2024@gmail.com>
2024-08-08 09:23:32 +0200

    This is my third commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/sof/",
+  base: "onlysupcall.github.io/sof/",
   plugins: [
     vue(),
   ],
